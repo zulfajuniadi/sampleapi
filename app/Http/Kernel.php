40bf002bd441fb8c6f3de1ecd8b533d1c036f451
Zulfa Juniadi bin Zulkifli <zulfajuniadi@gmail.com>
2016-03-23 23:59:20 +0800

    Add redirect on production

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        Middleware\RedirectHttps::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     ];
 
